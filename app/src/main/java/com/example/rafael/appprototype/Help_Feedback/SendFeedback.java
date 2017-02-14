0755a0f4416e56ab2ecb75a79a51ae0be1397aed
Felgueiras <rafaelfelgueiras1993@gmail.com>
2017-02-14 16:29:53 +0000

    created feedback capability (send email to developer)

--- app/src/main/java/com/example/rafael/appprototype/Help_Feedback/SendFeedback.java
+++ app/src/main/java/com/example/rafael/appprototype/Help_Feedback/SendFeedback.java
@@ -1,25 +1,92 @@
 package com.example.rafael.appprototype.Help_Feedback;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rafael.appprototype.R;
 
 
 public class SendFeedback extends Fragment {
 
+    private View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.send_feedback, container, false);
+        view = inflater.inflate(R.layout.send_feedback, container, false);
 
         // set the title
         getActivity().setTitle(getResources().getString(R.string.send_feedback));
-        return v;
+
+        final Button sendFeedback = (Button) view.findViewById(R.id.ButtonSendFeedback);
+        sendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmailWithFeedback();
+            }
+
+
+        });
+
+        return view;
+    }
+
+    private void sendEmailWithFeedback() {
+
+
+        final EditText nameField = (EditText) view.findViewById(R.id.EditTextName);
+        String name = nameField.getText().toString();
+
+        final EditText feedbackField = (EditText) view.findViewById(R.id.EditTextFeedbackBody);
+        String feedback = feedbackField.getText().toString();
+
+        final CheckBox responseCheckbox = (CheckBox) view.findViewById(R.id.CheckBoxResponse);
+        boolean bRequiresResponse = responseCheckbox.isChecked();
+
+        if (name.isEmpty()) {
+            Snackbar.make(view, getResources().getString(R.string.feedback_name_error), Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if (feedback.isEmpty()) {
+            Snackbar.make(view, getResources().getString(R.string.feedback_message_error), Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        String[] TO = {"rafaelfelgueiras1993@gmail.com"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        if (bRequiresResponse) {
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Prototype application feedback - requires response");
+        } else {
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Prototype application feedback");
+        }
+        emailIntent.putExtra(Intent.EXTRA_TEXT, feedback);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(), "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
